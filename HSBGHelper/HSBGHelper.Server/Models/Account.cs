7229c65dc0c7cea2153d83729152f3f93a0e122e
Zack Morgenthaler <zackmorgenthaler@gmail.com>
2024-08-04 07:32:47 -0500

    getting ready for lettuceencrypt

--- HSBGHelper/HSBGHelper.Server/Models/Account.cs
+++ HSBGHelper/HSBGHelper.Server/Models/Account.cs
@@ -1,14 +1,9 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-
-namespace HSBGHelper.Server.Models
+﻿namespace HSBGHelper.Server.Models
 {
-    public class Account
+    public class Account 
     {
-        [Key]
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public string Email { get; set; }
     }
 }
